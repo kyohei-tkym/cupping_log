ccc49ce2f10ad57db7e0dfca517862ce7d146839
kyohei-tkym <kyohe1208@gmail.com>
2021-10-02 07:35:06 +0000

    user signin/show/edit記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a820b7c6ed0cde1a9632558efaab39fe48cba5855c1b4b6a7a69b88d6306c46d0ab212bd24b8c71f03bfedd1c0bdac0906a4a36d7f3adb6a74d4305002b9f95b'
