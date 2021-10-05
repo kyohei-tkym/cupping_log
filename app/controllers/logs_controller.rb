07b2ccf45d6b59d194f0c044d28c0623f5d6047d
kyohei-tkym <kyohe1208@gmail.com>
2021-10-05 07:12:50 +0000

    Log機能　星評価以外完了

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,42 @@
+class LogsController < ApplicationController
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+    @log.user_id = current_user.id
+    @log.save
+    redirect_to logs_path
+  end
+
+  def index
+    @logs = Log.all
+  end
+
+  def show
+    @log = Log.find(params[:id])
+  end
+
+  def edit
+    @log = Log.find(params[:id])
+  end
+
+  def update
+    @log = Log.find(params[:id])
+    @log.update(log_params)
+    redirect_to log_path(log.id)
+  end
+
+  def destroy
+    @log= Log.find(params[:id])
+    @log.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:log).permit(:coffee_name, :shop_name, :price, :drink_date, :brew, :roast, :taste, :body, :memo, :rate, log_images_images: [])
+  end
+end
